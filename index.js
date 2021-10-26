ca003d3d867b154ce07387337d46d2bc57e4d282
CedricBouche <cedric.bouche@student.ehb.be>
2021-10-26 13:02:18 +0200

    clean up

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Get the API
  * 
@@ -18,35 +17,3 @@ async function getapi(url) {
 }
 
 getapi(api_url);
-
-=======
-const express = require("express");
-const server = express();
-const PORT = 3000;
-
-const owners = [{
-  name: "hello",
-  email: "hello@ehb.be"
-}];
-
-/**
- * [GET] /
- * returns "hello world" upon get request
- * @returns {object} with "message" param containing "hello world"
- */
-server.get("/",(req, res) =>{
-  res.send({message: "hello world"})
-})
-
-/**
- * [GET] /owner
- * @returns {Object} current owner object
- */
- server.get("/owner", (req, res) => {
-  res.json(owner);
-})
-
-server.listen(PORT, () => {
-  console.log(`server listening at port ${PORT}`);
-});
->>>>>>> fd77bc6b1922350b92113404f4302e584602faf7
