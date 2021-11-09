8b42ec5e99a17c1c11d54185bca8e402789aed1e
CedricBouche <cedric.bouche@student.ehb.be>
2021-11-09 16:04:30 +0100

    edit paths

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const express = require("express");
-const PORT = 3000;
+const PORT = 4000;
 const server = express();
 
 
 
-server.get("/footbal/movie", function(req,res){
+server.get("/", function(req,res){
   res.send("Hello");
 });
 
-server.put("/", function(req,res){
-  res.send("Hello");
-});
 
-server.delete("/", function(req,res){
-  res.send("Hello");
-});
 
 server.listen(PORT,() =>{
   console.log(`server is listening on port ${PORT}`);
 })
+
+
+
+
 
