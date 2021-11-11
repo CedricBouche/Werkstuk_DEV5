2ba556f17a797011b4b3fbd71737f0a7ecbc0e29
CedricBouche <cedric.bouche@student.ehb.be>
2021-11-11 19:13:35 +0100

    adding conventies

--- API/index.js
+++ API/index.js
@@ -3,6 +3,11 @@ const PORT = 4000;
 const server = express();
 
 
+/**
+ * [GET] test endpoint
+ * @returns (string) "hello world" if server is active
+ */
+
 server.get("/", function(req,res){
   res.send("Hello");
 });
