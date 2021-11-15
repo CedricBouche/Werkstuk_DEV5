8d336fa429b0159fd3c9e5f689278f8945cd1e8c
CedricBouche <cedric.bouche@student.ehb.be>
2021-11-15 20:56:44 +0100

    creating routes

--- API/src/index.js
+++ API/src/index.js
@@ -2,9 +2,48 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const pg = require('knex')({
+  client: 'pg',
+  connection: 'postgres://postgres:admin@localhost:5432/werkstukDev5',
+  searchPath: ['knex', 'public'],
+});
+
+/**
+ * [GET] /
+ * @returns {string} "Docker is working"
+ */
 app.get('/', (req, res) => {
   res.send('Docker is working!')
 })
+
+
+/**
+ * [GET] /api/users
+ * show a list of all the users from the database
+ * @returns {json} "Docker is working"
+ */
+ app.get('/api/users',  (req, res) => {
+  res.json(users);
+})
+
+/**
+ * [delete] /user
+ * deletes one user based on id
+ * @argument {id}
+ * @returns deleted user
+ */
+ app.delete('/api/users/:id', (req, res) => {
+  res.send('User is deleted based on id')
+})
+
+/**
+    * updates one user based on id
+    * @argument {id} 
+    * @returns changed user
+    */
+ app.put('/api/users/:id', (req, res) => {
+  res.send("User is changed based on id");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
